96521a210138ec1b74345ea205937884647cfe84
Denis Zaratan Pasin <zaratan@hey.com>
2022-08-29 10:53:02 +0200

    Init

---
+++ lib/m2m_keygen.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "m2m_keygen/version"
+
+module M2mKeygen
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/m2m_keygen_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe M2mKeygen do
+  it "has a version number" do
+    expect(M2mKeygen::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
