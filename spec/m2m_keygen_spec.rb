4dad008ef01e2eb81d8d2f50819a284a9bebfe95
Denis Zaratan Pasin <zaratan@hey.com>
2022-08-29 11:31:07 +0200

    Stupid basic test

--- spec/m2m_keygen_spec.rb
+++ spec/m2m_keygen_spec.rb
@@ -5,8 +5,4 @@ RSpec.describe M2mKeygen do
   it "has a version number" do
     expect(M2mKeygen::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
